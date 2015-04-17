59bdc54401a71b460893f478152d1ad24cf28415
Parker Zink <parker.zink@gmail.com>
2015-04-17 09:15:20 -0700

    Upgraded clock
    Upgraded by time clock to digital mode!

--- index.php
+++ index.php
@@ -72,25 +72,51 @@ and open the template in the editor.
        });
     });
     </script>
-    <script type="text/javascript">
-
-function GetClock(){
-d = new Date();
-nhour  = d.getHours();
-nmin   = d.getMinutes();
-nsec   = d.getSeconds();
-     if(nhour ==  0) {ap = " AM";nhour = 12;} 
-else if(nhour <= 11) {ap = " AM";} 
-else if(nhour == 12) {ap = " PM";} 
-else if(nhour >= 13) {ap = " PM";nhour -= 12;}
-
-if(nmin <= 9) {nmin = "0" +nmin;}
-
-
-document.getElementById('clockbox').innerHTML=" "+nhour+":"+nmin+":"+nsec+" "+ap+" ";
-setTimeout("GetClock()", 1000);
+    <style>
+.clockStyle {
+	background-color:#000;
+	border:#999 2px inset;
+	padding:6px;
+	color:#0FF;
+	font-family:"Arial Black", Gadget, sans-serif;
+    font-size:16px;
+    font-weight:bold;
+	letter-spacing: 2px;
+	display:inline;
 }
-window.onload=GetClock;
+</style>
+</head>
+<body>
+<div id="clockDisplay" class="clockStyle"></div>
+<script>
+function renderTime() {
+	var currentTime = new Date();
+	var diem = "AM";
+	var h = currentTime.getHours();
+	var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
+	setTimeout('renderTime()',1000);
+    if (h == 0) {
+		h = 12;
+	} else if (h > 12) { 
+		h = h - 12;
+		diem="PM";
+	}
+	if (h < 10) {
+		h = "0" + h;
+	}
+	if (m < 10) {
+		m = "0" + m;
+	}
+	if (s < 10) {
+		s = "0" + s;
+	}
+    var myClock = document.getElementById('clockDisplay');
+	myClock.textContent = h + ":" + m + ":" + s + " " + diem;
+	myClock.innerText = h + ":" + m + ":" + s + " " + diem;
+}
+renderTime();
 </script>
-<div id="clockbox"></div>
+</body>
+</html>
 </html>
